2f7fbd4da1371cbc590a5985bcd698dd8b9952f1
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-02-06 02:21:41 +0300

    reports

--- stock_report.py
+++ stock_report.py
@@ -1,3 +1,9 @@
 import os
+import logging
+from datetime import datetime, timedelta
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 import yfinance as yf
 from dotenv import load_dotenv
